544c938661ea17a3a098a3c7e3582000a9ce52c8
AnsuPandey <anshupandey1998@gmail.com>
2020-08-24 12:09:32 +0530

    Updates

---
+++ precache-manifest.36d9008a03ade218ac5d15356cc140c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d8590a188334f1afeecd6eaa4b66ce",
+    "url": "/Pagination-React/index.html"
+  },
+  {
+    "revision": "9a06558c4428645612ae",
+    "url": "/Pagination-React/static/css/main.110b7eaf.chunk.css"
+  },
+  {
+    "revision": "26f2fd292f7caf31b833",
+    "url": "/Pagination-React/static/js/2.012a9865.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pagination-React/static/js/2.012a9865.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a06558c4428645612ae",
+    "url": "/Pagination-React/static/js/main.4f3e362a.chunk.js"
+  },
+  {
+    "revision": "2f009897f7d8842b62d6",
+    "url": "/Pagination-React/static/js/runtime-main.45342ada.js"
+  }
+]);
